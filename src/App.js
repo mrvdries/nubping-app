3c1f23a7061ae64016480916dfa4d9d6470fab8d
mrvdries <van-driessche-maxime@hotmail.com>
2019-02-24 01:35:44 +0100

    fancy title screen

--- src/App.js
+++ src/App.js
@@ -7,18 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="title"><div className="title-no-anim no-pad">Nub</div><div className="title-flip-down no-pad">p</div><div className="title-move-down no-pad">ing</div></div>
         </header>
       </div>
     );
